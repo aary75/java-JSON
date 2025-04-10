116671ab7ea2ef3622c1d541400f4d37cf7c74e2
Aaryan <aaryankgarg@gmail.com>
2025-04-10 14:49:32 +0530

    [Aaryan] Reading the JSON file and extract specific elements

--- src/main/java/com/bridgelabz/javaJSON/PracticeProblems/ReadJSONfile.java
+++ src/main/java/com/bridgelabz/javaJSON/PracticeProblems/ReadJSONfile.java
@@ -1,4 +1,66 @@
 package com.bridgelabz.javaJSON.PracticeProblems;
 
-public class ReadJSONfile {
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+class Student{
+    private String name;
+    private String email;
+    private int age;
+    private String department;
+
+    public Student(){}
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
 }
+
+public class ReadJSONfile {
+     public static void main(String[] args){
+         try{
+             // Creating of objectMapper
+             ObjectMapper objectMapper = new ObjectMapper();
+
+             // Reading the file
+             Student student = objectMapper.readValue(new File("src/main/java/com/bridgelabz/javaJSON/PracticeProblems/data.json"),Student.class);
+
+             System.out.println("Name: " + student.getName());
+             System.out.println("Email: "+ student.getEmail());
+
+             // Another way
+//             ObjectMapper mapper = new ObjectMapper();
+//
+//             // Read JSON file into JsonNode
+//             JsonNode rootNode = mapper.readTree(new File("data.json"));
+//
+//             // Extract specific fields
+//             String name = rootNode.path("name").asText();
+//             String email = rootNode.path("email").asText();
+//
+//             // Print the extracted fields
+//             System.out.println("Name: " + name);
+//             System.out.println("Email: " + email);
+
+         } catch (Exception e){
+             e.printStackTrace();
+         }
+     }
+}
+
+//Name: Aaryan
+//Email: aaryan@gmail.com
